8bfa109414f4d91e8e176e78bae5d50ddbbdb14b
Joanna Skurzyńska <joanna.skurzynska@hotmail.com>
2017-03-21 23:22:01 +0100

    First step of building API - get item

---
+++ HotSwap/Controllers/ItemsController.cs
@@ -0,0 +1,14 @@
+﻿using HotSwap.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotSwap.Controllers
+{
+    public class ItemsController : Controller
+    {
+        [HttpGet("api/item")]
+        public JsonResult GetItem()
+        {
+            return new JsonResult(new ItemDto{ Id = 1, Name = "first"});
+        }
+    }
+}
--- HotSwap/Startup.cs
+++ HotSwap/Startup.cs
@@ -16,6 +16,7 @@ namespace HotSwap
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +28,8 @@ namespace HotSwap
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
